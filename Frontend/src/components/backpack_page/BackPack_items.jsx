791d100d6637dda5f30bc032f3a696ddd29031e7
Purple-Snake <titas.sniuksta@gmail.com>
2023-12-19 19:56:09 +0200

    added links to kuriniai

--- Frontend/src/components/backpack_page/BackPack_items.jsx
+++ Frontend/src/components/backpack_page/BackPack_items.jsx
@@ -3,7 +3,7 @@ import BackPack_item from "./backPack_item";
 
 function BackPack_items() {
     return ( 
-        <div class="accordeon">
+        <div className="accordeon">
             {backpackData.map(data => (
                 <BackPack_item  data={data} key={data.id}/>
             ))}
--- Frontend/src/components/backpack_page/backPack_item.jsx
+++ Frontend/src/components/backpack_page/backPack_item.jsx
@@ -10,7 +10,22 @@ function BackPack_item({ data }) {
           <Accordion>
             <Accordion.Item eventKey="0">
               <Accordion.Header>{contentData.header}</Accordion.Header>
-              <Accordion.Body>{contentData.body}</Accordion.Body>
+              <Accordion.Body>
+                {contentData.body}
+                {contentData.links && contentData.links.length > 0 && (
+                  <span>
+                    {contentData.links.map((link, linkIndex) => (
+                        <a key={linkIndex}
+                          href={link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Link {linkIndex + 1}
+                        </a>
+                    ))}
+                  </span>
+                )}
+              </Accordion.Body>
             </Accordion.Item>
           </Accordion>
         </div>
